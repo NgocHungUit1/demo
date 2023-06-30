5869786dee42a17232abb349634df7a2349fb785
hduong25 <18520270@gm.uit.edu.vn>
2023-06-30 22:57:40 +0700

    fix API view chapter

--- backend/MangaWebSite/app/Http/Controllers/Client/ViewChapterController.php
+++ backend/MangaWebSite/app/Http/Controllers/Client/ViewChapterController.php
@@ -9,20 +9,12 @@ use App\Models\Page;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
+
 class ViewChapterController extends Controller
 {
     public function pageOfChapter($chapter_id)
     {
         $chapter = Chapter::findOrFail($chapter_id);
-        //page là ảnh của một tập truyện cụ thể
-        $pages = $chapter->pages;
-        // Get the manga
-        $mangaId = $chapter->manga_id;
-        $manga = Manga::findOrFail($mangaId);
-
-        // Get all chapters of the manga
-        $allChapters = Chapter::where('manga_id', $mangaId)->get();
-
         // Get the next chapter
         $nextChapter = Chapter::where('manga_id', $chapter->manga_id)
             ->where('order', '>', $chapter->order)
@@ -37,13 +29,13 @@ class ViewChapterController extends Controller
 
         // Prepare the response data
         $responseData = [
-            'manga_name' => $manga->name,
-            'all_chapters' => $allChapters,
             'chapter' => $chapter,
-            'pages' => $pages,
+            'pages' => $chapter->pages,
             'next_chapter' => $nextChapter,
             'previous_chapter' => $previousChapter,
         ];
+
+        unset($responseData['pages']); // Remove the duplicate "pages" key
 
         return response()->json(['data' => $responseData]);
     }
