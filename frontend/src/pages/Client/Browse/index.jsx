130713e58e062ef12de5fa1e594835dfbb1daf19
iiamhyy <vuthithuhong.psn@gmail.com>
2023-07-10 16:54:00 +0700

    browse

--- frontend/src/pages/Client/Browse/index.jsx
+++ frontend/src/pages/Client/Browse/index.jsx
@@ -1,16 +1,44 @@
 import classNames from "classnames/bind";
 import styles from "./Browse.module.scss";
 import { Select } from "antd";
+import { useEffect, useState } from "react";
+import { ReactComponent as Gird1Icon } from "@/assets/images/Browse/grid-1.svg";
+import { ReactComponent as Grid2Icon } from "@/assets/images/Browse/grid-2.svg";
+import { ReactComponent as ListIcon } from "@/assets/images/Browse/list.svg";
 
 const cx = classNames.bind(styles);
 
 function Browse() {
+    const [mangas, setMangas] = useState([]);
+    const [genres, setGenres] = useState([]);
+
+    useEffect(() => {
+        async function fetchData() {
+            const response = await fetch("http://localhost:8000/api/genres");
+            const data = await response.json();
+            if (Array.isArray(data.data)) {
+                setGenres(data.data);
+            }
+        }
+        fetchData();
+    }, []);
+    useEffect(() => {
+        async function fetchData() {
+            const response = await fetch("http://localhost:8000/api/mangas");
+            const data = await response.json();
+            if (Array.isArray(data.data)) {
+                setMangas(data.data);
+            }
+        }
+        fetchData();
+    }, []);
+
     return (
         <div className={cx("wrapper")}>
             <div className={cx("inner")}>
                 <div className={cx("filter")}>
                     <div className={cx("item")}>
-                        <p>Thể loại</p>         
+                        <p>Thể loại</p>
                         <Select
                             showSearch
                             size="large"
@@ -26,51 +54,43 @@ function Browse() {
                                         (optionB?.label ?? "").toLowerCase()
                                     )
                             }
-                            options={[
-                                {
-                                    value: "1",
-                                    label: "Not Identified",
-                                },
-                                {
-                                    value: "2",
-                                    label: "Closed",
-                                },
-                                {
-                                    value: "3",
-                                    label: "Communicated",
-                                },
-                            ]}
+                            options={
+                                genres.map((genre) => ({
+                                    value: genre.id,
+                                    label: genre.name,
+                                }))
+                            }
                         />
                     </div>
                     <div className={cx("item")}>
                         <p>Sắp xếp</p>
                         <Select
-                            showSearch
+                            defaultValue="1"
                             size="large"
-                            placeholder="Thể loại..."
-                            optionFilterProp="children"
-                            filterOption={(input, option) =>
-                                (option?.label ?? "").includes(input)
-                            }
-                            filterSort={(optionA, optionB) =>
-                                (optionA?.label ?? "")
-                                    .toLowerCase()
-                                    .localeCompare(
-                                        (optionB?.label ?? "").toLowerCase()
-                                    )
-                            }
                             options={[
                                 {
                                     value: "1",
-                                    label: "Not Identified",
+                                    label: "Tất cả",
                                 },
                                 {
                                     value: "2",
-                                    label: "Closed",
+                                    label: "View tháng",
                                 },
                                 {
                                     value: "3",
-                                    label: "Communicated",
+                                    label: "View tuần",
+                                },
+                                {
+                                    value: "4",
+                                    label: "View ngày",
+                                },
+                                {
+                                    value: "3",
+                                    label: "Mới ra",
+                                },
+                                {
+                                    value: "4",
+                                    label: "Chapter mới",
                                 },
                             ]}
                         />
@@ -78,32 +98,20 @@ function Browse() {
                     <div className={cx("item")}>
                         <p>Trạng thái</p>
                         <Select
-                            showSearch
+                            defaultValue="1"
                             size="large"
-                            placeholder="Thể loại..."
-                            optionFilterProp="children"
-                            filterOption={(input, option) =>
-                                (option?.label ?? "").includes(input)
-                            }
-                            filterSort={(optionA, optionB) =>
-                                (optionA?.label ?? "")
-                                    .toLowerCase()
-                                    .localeCompare(
-                                        (optionB?.label ?? "").toLowerCase()
-                                    )
-                            }
                             options={[
                                 {
                                     value: "1",
-                                    label: "Not Identified",
+                                    label: "Tất cả",
                                 },
                                 {
                                     value: "2",
-                                    label: "Closed",
+                                    label: "Đang tiến hành",
                                 },
                                 {
                                     value: "3",
-                                    label: "Communicated",
+                                    label: "Hoàn thành",
                                 },
                             ]}
                         />
@@ -113,7 +121,7 @@ function Browse() {
                         <Select
                             showSearch
                             size="large"
-                            placeholder="Thể loại..."
+                            placeholder="Loại truyện..."
                             optionFilterProp="children"
                             filterOption={(input, option) =>
                                 (option?.label ?? "").includes(input)
@@ -128,15 +136,19 @@ function Browse() {
                             options={[
                                 {
                                     value: "1",
-                                    label: "Not Identified",
+                                    label: "Manga",
                                 },
                                 {
                                     value: "2",
-                                    label: "Closed",
+                                    label: "Manhua",
                                 },
                                 {
                                     value: "3",
-                                    label: "Communicated",
+                                    label: "manhwa",
+                                },
+                                {
+                                    value: "3",
+                                    label: "doujinshi",
                                 },
                             ]}
                         />
@@ -144,32 +156,32 @@ function Browse() {
                     <div className={cx("item")}>
                         <p>Số chương</p>
                         <Select
-                            showSearch
+                            defaultValue="1"
                             size="large"
-                            placeholder="Thể loại..."
-                            optionFilterProp="children"
-                            filterOption={(input, option) =>
-                                (option?.label ?? "").includes(input)
-                            }
-                            filterSort={(optionA, optionB) =>
-                                (optionA?.label ?? "")
-                                    .toLowerCase()
-                                    .localeCompare(
-                                        (optionB?.label ?? "").toLowerCase()
-                                    )
-                            }
                             options={[
                                 {
                                     value: "1",
-                                    label: "Not Identified",
+                                    label: ">0",
                                 },
                                 {
                                     value: "2",
-                                    label: "Closed",
+                                    label: ">=100",
                                 },
                                 {
                                     value: "3",
-                                    label: "Communicated",
+                                    label: ">=200",
+                                },
+                                {
+                                    value: "4",
+                                    label: ">=300",
+                                },
+                                {
+                                    value: "5",
+                                    label: ">=400",
+                                },
+                                {
+                                    value: "6",
+                                    label: ">=500",
                                 },
                             ]}
                         />
@@ -177,40 +189,43 @@ function Browse() {
                     <div className={cx("item")}>
                         <p>Giới tính</p>
                         <Select
-                            showSearch
+                            defaultValue="1"
                             size="large"
-                            placeholder="Thể loại..."
-                            optionFilterProp="children"
-                            filterOption={(input, option) =>
-                                (option?.label ?? "").includes(input)
-                            }
-                            filterSort={(optionA, optionB) =>
-                                (optionA?.label ?? "")
-                                    .toLowerCase()
-                                    .localeCompare(
-                                        (optionB?.label ?? "").toLowerCase()
-                                    )
-                            }
                             options={[
                                 {
                                     value: "1",
-                                    label: "Not Identified",
+                                    label: "Tất cả",
                                 },
                                 {
                                     value: "2",
-                                    label: "Closed",
+                                    label: "Con gái",
                                 },
                                 {
                                     value: "3",
-                                    label: "Communicated",
+                                    label: "Con trai",
                                 },
                             ]}
                         />
                     </div>
                 </div>
-                <div className={cx("grid")}></div>
+                <div className={cx("grid")}>
+                    <button>
+                        <Gird1Icon />
+                    </button>
+                    <button>
+                        <Grid2Icon />
+                    </button>
+                    <button>
+                        <ListIcon />
+                    </button>
+                </div>
                 <div className={cx("result")}>
-                    
+                    {mangas?.map((manga) => (
+                        <div className={cx("manga")}>
+                            <img src={manga.image} alt="" />
+                            <p>{manga.name}</p>
+                        </div>
+                    ))}
                 </div>
             </div>
         </div>
--- frontend/src/pages/Client/Manga/index.jsx
+++ frontend/src/pages/Client/Manga/index.jsx
@@ -1,6 +1,6 @@
 import classNames from "classnames/bind";
 import styles from "./Manga.module.scss";
-import { Col, Input, Modal, Row } from "antd";
+import { Avatar, Col, Input, Modal, Row } from "antd";
 import {
     UserOutlined,
     ArrowUpOutlined,
@@ -11,6 +11,8 @@ import { useState, useEffect } from "react";
 import { ReactComponent as BookOutlineIcon } from "@/assets/images/Manga/book-ouline.svg";
 import { ReactComponent as NewestIcon } from "@/assets/images/Manga/newst-icon.svg";
 import { ReactComponent as BookIcon } from "@/assets/images/Manga/book-icon.svg";
+import { ReactComponent as ReplyIcon } from "@/assets/images/Manga/reply.svg";
+import { ReactComponent as ActionIcon } from "@/assets/images/Manga/more-horizontal-svgrepo-com.svg";
 import { ReactComponent as SaveIcon } from "@/assets/images/Manga/save-icon.svg";
 import { ReactComponent as BellOutlineIcon } from "@/assets/images/Manga/bell-outline.svg";
 import { useLocation } from "react-router-dom";
@@ -44,7 +46,7 @@ function Manga() {
 
     const [isFollowOpen, setIsFollowOpen] = useState(false);
     const { client } = useSelector((st) => st.client);
-    console.log(mangaData)
+    console.log(mangaData);
     return (
         <div className={cx("wrapper")}>
             <Row style={{ position: "relative", height: "450px" }}>
@@ -212,11 +214,59 @@ function Manga() {
                                 bạn nhé
                             </div>
                         )}
-<div className={cx("comment-wrapper")}>
-    {mangaData.comments.map(comment => (
-        <div className={cx("comment")} key={comment.id}>{comment.id}</div>
-    ) )}
-</div>
+                        <div className={cx("comment-wrapper")}>
+                            {mangaData.comments.map((comment) => (
+                                <div className={cx("comment")} key={comment.id}>
+                                    <div className={cx("user-action")}>
+                                        <div className={cx("user")}>
+                                            {comment.user.image ? (
+                                                <Avatar
+                                                    size={50}
+                                                    src={comment.user.image}
+                                                />
+                                            ) : (
+                                                <Avatar
+                                                    size={50}
+                                                    icon={<UserOutlined />}
+                                                    style={{
+                                                        backgroundColor:
+                                                            "rgb(63,63,63)",
+                                                    }}
+                                                />
+                                            )}
+                                            <p>{comment.user.name}</p>
+                                        </div>
+                                        <div className={cx("action")}>
+                                            <button><ActionIcon/></button>
+                                        </div>
+                                    </div>
+                                    <p className={cx("des")}>{comment.comment}</p>
+                                    <div className={cx("reply")}>
+                                        <div className={cx("emoji")}>
+                                            <button>
+                                                <ReplyIcon />
+                                            </button>
+                                        </div>
+                                        <div className={cx("emoji")}>
+                                            <button>👍</button>
+                                            <p>0</p>
+                                        </div>
+                                        <div className={cx("emoji")}>
+                                            <button>❤</button>
+                                            <p>0</p>
+                                        </div>
+                                        <div className={cx("emoji")}>
+                                            <button>🤡</button>
+                                            <p>0</p>
+                                        </div>
+                                        <div className={cx("emoji")}>
+                                            <button>😡</button>
+                                            <p>0</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            ))}
+                        </div>
                     </Col>
                 </Row>
             ) : (
